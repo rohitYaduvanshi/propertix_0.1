46a7c0cf9657ecf9ec027ced043d50f4f025edbe
ROHIT KUMAR <rohitkumarpc79@gmail.com>
2026-02-15 11:52:25 +0530

    commit

--- src/blockchain/contractConfig.js
+++ src/blockchain/contractConfig.js
@@ -2,7 +2,7 @@
 import PropertyRegistry from "./PropertyRegistry.json"; 
 
 // ✅ Step 2: Naya Contract Address 
-export const PROPERTY_REGISTRY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+export const PROPERTY_REGISTRY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // ✅ Step 3: ABI ab direct JSON file se aayega (No manual copy-paste needed)
 export const PROPERTY_REGISTRY_ABI = PropertyRegistry.abi;
