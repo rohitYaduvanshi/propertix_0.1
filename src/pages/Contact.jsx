1896a13b64b240302fd178a445debc8f519325da
ROHIT KUMAR <rohitkumarpc79@gmail.com>
2026-02-18 00:02:17 +0530

    commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,7 @@ const Contact = () => {
     <div className="px-8 py-10">
       <h2 className="text-2xl font-semibold mb-4">Contact</h2>
       <p className="text-gray-300 text-sm max-w-2xl">
-        Hackathon / demo ke liye yaha apna naam, college, team details, GitHub
-        repo link, aur email add kar sakte ho.
+        Under Developing........!
       </p>
     </div>
   );
