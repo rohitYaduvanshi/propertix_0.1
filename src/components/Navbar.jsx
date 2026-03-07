93107dd611a0198c107ce60bd880c3818e0648cf
ROHIT KUMAR <rohitkumarpc79@gmail.com>
2026-03-07 09:35:55 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
         <div className="hidden lg:flex items-center gap-8">
             <NavLink to="/" className={linkClasses}>Home</NavLink>
             <NavLink to="/registerAsset" className={linkClasses}>RegisterAsset</NavLink>
-            <NavLink to="/TransferOwnership" className={linkClasses}>GiftOwnership</NavLink>
+            <NavLink to="/transferOwnership" className={linkClasses}>GiftOwnership</NavLink>
             <NavLink to="/map" className={linkClasses}>Property-Map</NavLink>
             <NavLink to="/about" className={linkClasses}>About</NavLink>
             <NavLink to="/contact" className={linkClasses}>Contact</NavLink>
@@ -242,6 +242,7 @@ const Navbar = () => {
                 <div className="flex flex-col space-y-2">
                     <NavLink to="/" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>Home</NavLink>
                     <NavLink to="/registerAsset" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>RegisterAsset</NavLink>
+                    <NavLink to="/transferOwnership" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>GiftOwnership</NavLink>
                     <NavLink to="/map" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>Property-Map</NavLink>
                     <NavLink to="/about" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>About</NavLink>
                     <NavLink to="/contact" className={mobileLinkClasses} onClick={() => setIsMenuOpen(false)}>Contact</NavLink>
